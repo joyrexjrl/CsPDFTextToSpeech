c6b5eb9308198ae08752943fbceffb189ca67ce6
Rick Lozano <ricklozano@gmail.com>
2025-06-02 14:50:55 -0700

    Initial commit

---
+++ Bed Time Story/Bed Time Story/Form1.Designer.cs
@@ -0,0 +1,324 @@
+﻿
+namespace Bed_Time_Story
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.playButton = new System.Windows.Forms.Button();
+            this.pauseButton = new System.Windows.Forms.Button();
+            this.stopButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.loadPdfButton = new System.Windows.Forms.Button();
+            this.loadedPdfTextbox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.startPageTextbox = new System.Windows.Forms.TextBox();
+            this.endPageTextbox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.loadedPdfTotalPageLabel = new System.Windows.Forms.Label();
+            this.clearPdfButton = new System.Windows.Forms.Button();
+            this.displayPdfTextbox = new System.Windows.Forms.TextBox();
+            this.pdfWebBrowser = new Microsoft.Web.WebView2.WinForms.WebView2();
+            this.voiceSelectCombobox = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.talkSpeedSlider = new System.Windows.Forms.TrackBar();
+            this.talkVolumeSlider = new System.Windows.Forms.TrackBar();
+            this.customTextCheckBox = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pdfWebBrowser)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.talkSpeedSlider)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.talkVolumeSlider)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // playButton
+            // 
+            this.playButton.Location = new System.Drawing.Point(12, 798);
+            this.playButton.Name = "playButton";
+            this.playButton.Size = new System.Drawing.Size(75, 23);
+            this.playButton.TabIndex = 0;
+            this.playButton.Text = "Play";
+            this.playButton.UseVisualStyleBackColor = true;
+            this.playButton.Click += new System.EventHandler(this.playButton_Click);
+            // 
+            // pauseButton
+            // 
+            this.pauseButton.Location = new System.Drawing.Point(93, 798);
+            this.pauseButton.Name = "pauseButton";
+            this.pauseButton.Size = new System.Drawing.Size(75, 23);
+            this.pauseButton.TabIndex = 1;
+            this.pauseButton.Text = "Pause";
+            this.pauseButton.UseVisualStyleBackColor = true;
+            this.pauseButton.Click += new System.EventHandler(this.pauseButton_Click);
+            // 
+            // stopButton
+            // 
+            this.stopButton.Location = new System.Drawing.Point(174, 798);
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(75, 23);
+            this.stopButton.TabIndex = 2;
+            this.stopButton.Text = "Stop";
+            this.stopButton.UseVisualStyleBackColor = true;
+            this.stopButton.Click += new System.EventHandler(this.stopButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(966, 798);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 23);
+            this.exitButton.TabIndex = 3;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // loadPdfButton
+            // 
+            this.loadPdfButton.Location = new System.Drawing.Point(12, 12);
+            this.loadPdfButton.Name = "loadPdfButton";
+            this.loadPdfButton.Size = new System.Drawing.Size(75, 23);
+            this.loadPdfButton.TabIndex = 4;
+            this.loadPdfButton.Text = "Load PDF";
+            this.loadPdfButton.UseVisualStyleBackColor = true;
+            this.loadPdfButton.Click += new System.EventHandler(this.loadPdfButton_Click);
+            // 
+            // loadedPdfTextbox
+            // 
+            this.loadedPdfTextbox.Location = new System.Drawing.Point(93, 41);
+            this.loadedPdfTextbox.Name = "loadedPdfTextbox";
+            this.loadedPdfTextbox.Size = new System.Drawing.Size(191, 20);
+            this.loadedPdfTextbox.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Loaded PDF";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Read text from:";
+            // 
+            // startPageTextbox
+            // 
+            this.startPageTextbox.Location = new System.Drawing.Point(94, 72);
+            this.startPageTextbox.Name = "startPageTextbox";
+            this.startPageTextbox.Size = new System.Drawing.Size(100, 20);
+            this.startPageTextbox.TabIndex = 8;
+            // 
+            // endPageTextbox
+            // 
+            this.endPageTextbox.Location = new System.Drawing.Point(225, 72);
+            this.endPageTextbox.Name = "endPageTextbox";
+            this.endPageTextbox.Size = new System.Drawing.Size(100, 20);
+            this.endPageTextbox.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(200, 75);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(19, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "to:";
+            // 
+            // loadedPdfTotalPageLabel
+            // 
+            this.loadedPdfTotalPageLabel.AutoSize = true;
+            this.loadedPdfTotalPageLabel.Location = new System.Drawing.Point(290, 44);
+            this.loadedPdfTotalPageLabel.Name = "loadedPdfTotalPageLabel";
+            this.loadedPdfTotalPageLabel.Size = new System.Drawing.Size(35, 13);
+            this.loadedPdfTotalPageLabel.TabIndex = 11;
+            this.loadedPdfTotalPageLabel.Text = "label4";
+            // 
+            // clearPdfButton
+            // 
+            this.clearPdfButton.Location = new System.Drawing.Point(94, 12);
+            this.clearPdfButton.Name = "clearPdfButton";
+            this.clearPdfButton.Size = new System.Drawing.Size(75, 23);
+            this.clearPdfButton.TabIndex = 12;
+            this.clearPdfButton.Text = "Clear PDF";
+            this.clearPdfButton.UseVisualStyleBackColor = true;
+            this.clearPdfButton.Click += new System.EventHandler(this.clearPdfButton_Click);
+            // 
+            // displayPdfTextbox
+            // 
+            this.displayPdfTextbox.Location = new System.Drawing.Point(15, 98);
+            this.displayPdfTextbox.Multiline = true;
+            this.displayPdfTextbox.Name = "displayPdfTextbox";
+            this.displayPdfTextbox.ReadOnly = true;
+            this.displayPdfTextbox.Size = new System.Drawing.Size(503, 684);
+            this.displayPdfTextbox.TabIndex = 13;
+            // 
+            // pdfWebBrowser
+            // 
+            this.pdfWebBrowser.AllowExternalDrop = true;
+            this.pdfWebBrowser.CreationProperties = null;
+            this.pdfWebBrowser.DefaultBackgroundColor = System.Drawing.Color.White;
+            this.pdfWebBrowser.Location = new System.Drawing.Point(535, 98);
+            this.pdfWebBrowser.Name = "pdfWebBrowser";
+            this.pdfWebBrowser.Size = new System.Drawing.Size(506, 684);
+            this.pdfWebBrowser.TabIndex = 14;
+            this.pdfWebBrowser.ZoomFactor = 1D;
+            // 
+            // voiceSelectCombobox
+            // 
+            this.voiceSelectCombobox.FormattingEnabled = true;
+            this.voiceSelectCombobox.Location = new System.Drawing.Point(363, 14);
+            this.voiceSelectCombobox.Name = "voiceSelectCombobox";
+            this.voiceSelectCombobox.Size = new System.Drawing.Size(171, 21);
+            this.voiceSelectCombobox.TabIndex = 15;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(290, 17);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 13);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "Voice Select";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(331, 62);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(62, 13);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "Talk Speed";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(708, 62);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(66, 13);
+            this.label6.TabIndex = 18;
+            this.label6.Text = "Talk Volume";
+            // 
+            // talkSpeedSlider
+            // 
+            this.talkSpeedSlider.Location = new System.Drawing.Point(399, 47);
+            this.talkSpeedSlider.Minimum = -10;
+            this.talkSpeedSlider.Name = "talkSpeedSlider";
+            this.talkSpeedSlider.Size = new System.Drawing.Size(261, 45);
+            this.talkSpeedSlider.TabIndex = 19;
+            // 
+            // talkVolumeSlider
+            // 
+            this.talkVolumeSlider.LargeChange = 10;
+            this.talkVolumeSlider.Location = new System.Drawing.Point(780, 47);
+            this.talkVolumeSlider.Maximum = 100;
+            this.talkVolumeSlider.Name = "talkVolumeSlider";
+            this.talkVolumeSlider.Size = new System.Drawing.Size(261, 45);
+            this.talkVolumeSlider.SmallChange = 5;
+            this.talkVolumeSlider.TabIndex = 20;
+            this.talkVolumeSlider.Value = 100;
+            // 
+            // customTextCheckBox
+            // 
+            this.customTextCheckBox.AutoSize = true;
+            this.customTextCheckBox.Location = new System.Drawing.Point(277, 798);
+            this.customTextCheckBox.Name = "customTextCheckBox";
+            this.customTextCheckBox.Size = new System.Drawing.Size(108, 17);
+            this.customTextCheckBox.TabIndex = 21;
+            this.customTextCheckBox.Text = "Enter custom text";
+            this.customTextCheckBox.UseVisualStyleBackColor = true;
+            this.customTextCheckBox.CheckedChanged += new System.EventHandler(this.customTextCheckBox_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1053, 833);
+            this.Controls.Add(this.customTextCheckBox);
+            this.Controls.Add(this.talkVolumeSlider);
+            this.Controls.Add(this.talkSpeedSlider);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.voiceSelectCombobox);
+            this.Controls.Add(this.pdfWebBrowser);
+            this.Controls.Add(this.displayPdfTextbox);
+            this.Controls.Add(this.clearPdfButton);
+            this.Controls.Add(this.loadedPdfTotalPageLabel);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.endPageTextbox);
+            this.Controls.Add(this.startPageTextbox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.loadedPdfTextbox);
+            this.Controls.Add(this.loadPdfButton);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.stopButton);
+            this.Controls.Add(this.pauseButton);
+            this.Controls.Add(this.playButton);
+            this.Name = "Form1";
+            this.Text = "Bed Time Story";
+            ((System.ComponentModel.ISupportInitialize)(this.pdfWebBrowser)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.talkSpeedSlider)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.talkVolumeSlider)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button playButton;
+        private System.Windows.Forms.Button pauseButton;
+        private System.Windows.Forms.Button stopButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Button loadPdfButton;
+        private System.Windows.Forms.TextBox loadedPdfTextbox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox startPageTextbox;
+        private System.Windows.Forms.TextBox endPageTextbox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label loadedPdfTotalPageLabel;
+        private System.Windows.Forms.Button clearPdfButton;
+        private System.Windows.Forms.TextBox displayPdfTextbox;
+        private Microsoft.Web.WebView2.WinForms.WebView2 pdfWebBrowser;
+        private System.Windows.Forms.ComboBox voiceSelectCombobox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TrackBar talkSpeedSlider;
+        private System.Windows.Forms.TrackBar talkVolumeSlider;
+        private System.Windows.Forms.CheckBox customTextCheckBox;
+    }
+}
+
---
+++ Bed Time Story/Bed Time Story/Form1.cs
@@ -0,0 +1,275 @@
+﻿using System;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Speech.Synthesis;
+using System.Text.RegularExpressions;
+
+using PdfPigDoc = UglyToad.PdfPig.PdfDocument;
+using PdfiumDoc = PdfiumViewer.PdfDocument;
+
+namespace Bed_Time_Story
+{
+    public partial class Form1 : Form
+    {
+        SpeechSynthesizer _synthesizer = new SpeechSynthesizer();
+        PdfiumDoc _loadedDocument;
+        int _totalPages = 0;
+        int _currentCharIndex = 0;
+        string _fullTextToRead = "";
+        string _filePath = "";
+        bool _isPaused = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            loadedPdfTotalPageLabel.Text = "";
+            playButton.Enabled = false;
+            pauseButton.Enabled = false;
+            stopButton.Enabled = false;
+
+            PopulateVoiceOptions();
+
+            customTextCheckBox.CheckedChanged += customTextCheckBox_CheckedChanged;
+            displayPdfTextbox.TextChanged += displayPdfTextbox_TextChanged;
+            _synthesizer.SpeakCompleted += Synthesizer_SpeakCompleted;
+        }
+
+        void exitButton_Click(object sender, EventArgs e) => Close();
+
+        async void loadPdfButton_Click(object sender, EventArgs e)
+        {
+            using(OpenFileDialog dialog = new OpenFileDialog())
+            {
+                dialog.Filter = "FDP files (*.pdf)|*.pdf";
+                if(dialog.ShowDialog() == DialogResult.OK)
+                {
+                    if (customTextCheckBox.Checked)
+                    {
+                        customTextCheckBox.Checked = false;
+                        displayPdfTextbox.Text = "";
+                    }
+                    _filePath = dialog.FileName;
+                    string fileName = Path.GetFileName(_filePath);
+                    _loadedDocument?.Dispose();
+                    _loadedDocument = PdfiumDoc.Load(_filePath);
+                    _totalPages = _loadedDocument.PageCount;
+
+                    loadedPdfTotalPageLabel.Text = $"Max pages: {_totalPages}";
+                    loadedPdfTextbox.Text = $"Loaded: {fileName}";
+
+                    if (!HasTextLayer(_filePath)) MessageBox.Show("This PDF does not appear to contain a readable text layer. It may be an image-only scan.");
+
+                    await pdfWebBrowser.EnsureCoreWebView2Async(null);
+                    pdfWebBrowser.CoreWebView2.Navigate(new Uri(_filePath).AbsoluteUri);
+                    playButton.Enabled = true;
+                }
+            }            
+        }
+
+        string ExtractWithPdfPig(string filePath, int startPage, int endPage)
+        {
+            var textBuilder = new StringBuilder();
+
+            using (PdfPigDoc document = PdfPigDoc.Open(filePath))
+            {
+                for (int i = startPage; i <= endPage; i++)
+                {
+                    var page = document.GetPage(i);
+                    var lines = page.Text.Split(new[] { '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+                    foreach (var line in lines)
+                    {
+                        string trimmed = line.Trim();
+                        if (int.TryParse(trimmed, out int num) && num >= startPage && num <= endPage) continue;
+                        if (trimmed.ToLower().StartsWith("page ") || trimmed.ToLower().EndsWith("page")) continue;
+                        trimmed = Regex.Replace(trimmed, @"(?<![\s.,;:!?])([A-Z])", " $1");
+                        textBuilder.Append(trimmed);
+                        if (!trimmed.EndsWith(" ")) textBuilder.Append(" ");
+                    }
+                    textBuilder.AppendLine();
+                }
+            }
+            return textBuilder.ToString();
+        }
+
+        bool HasTextLayer(string filePath, int pagesToCheck = 3)
+        {
+            using (var document = PdfPigDoc.Open(filePath))
+            {
+                int pages = Math.Min(pagesToCheck, document.NumberOfPages);
+                for (int i = 1; i <= pages; i++)
+                {
+                    var pageText = document.GetPage(i).Text;
+                    if (!string.IsNullOrWhiteSpace(pageText)) return true;
+                }
+            }
+            return false;
+        }
+
+        void PopulateVoiceOptions()
+        {
+            voiceSelectCombobox.Items.Clear();
+            using (SpeechSynthesizer tempSynth = new SpeechSynthesizer())
+            {
+                foreach (InstalledVoice voice in tempSynth.GetInstalledVoices())
+                {
+                    var info = voice.VoiceInfo;
+                    voiceSelectCombobox.Items.Add(info.Name);
+                }
+            }
+
+            if (voiceSelectCombobox.Items.Count > 0) voiceSelectCombobox.SelectedIndex = 0;
+        }
+
+        void playButton_Click(object sender, EventArgs e)
+        {
+            string textToRead;
+
+            if (customTextCheckBox.Checked)
+            {
+                textToRead = displayPdfTextbox.Text;
+
+                if (string.IsNullOrWhiteSpace(textToRead))
+                {
+                    MessageBox.Show("Please enter custom text to read.");
+                    return;
+                }
+            }
+            else
+            {
+                if (_loadedDocument == null)
+                {
+                    MessageBox.Show("Please load a PDF first.");
+                    return;
+                }
+
+                bool hasStart = int.TryParse(startPageTextbox.Text, out int startPage);
+                bool hasEnd = int.TryParse(endPageTextbox.Text, out int endPage);
+
+                if (!hasStart && !hasEnd) textToRead = ExtractWithPdfPig(_filePath, 1, _totalPages);
+                else
+                {
+                    startPage = hasStart ? Clamp(startPage, 1, _totalPages) : 1;
+                    endPage = hasEnd ? Clamp(endPage, startPage, _totalPages) : _totalPages;
+                    textToRead = ExtractWithPdfPig(_filePath, startPage, endPage);
+                }
+                displayPdfTextbox.Text = textToRead;
+            }
+
+            _fullTextToRead = textToRead;
+            _currentCharIndex = 0;
+            _isPaused = false;
+
+            if (voiceSelectCombobox.SelectedItem != null) _synthesizer.SelectVoice(voiceSelectCombobox.SelectedItem.ToString());
+
+            _synthesizer.Rate = talkSpeedSlider.Value;
+            _synthesizer.Volume = talkVolumeSlider.Value;
+            _synthesizer.SpeakProgress -= Synthesizer_SpeakProgress;
+            _synthesizer.SpeakProgress += Synthesizer_SpeakProgress;
+            _synthesizer.SpeakAsync(textToRead);
+
+            pauseButton.Enabled = true;
+            stopButton.Enabled = true;
+            talkSpeedSlider.Enabled = false;
+            talkVolumeSlider.Enabled = false;
+        }
+
+
+        int Clamp(int value, int min, int max) => (value < min) ? min : (value > max) ? max : value;
+
+        void pauseButton_Click(object sender, EventArgs e)
+        {
+            if (!_isPaused)
+            {
+                _synthesizer.SpeakAsyncCancelAll();
+                _isPaused = true;
+                pauseButton.Text = "Unpause";
+            }
+            else
+            {
+                if(_currentCharIndex < _fullTextToRead.Length)
+                {
+                    _synthesizer.SpeakAsync(_fullTextToRead.Substring(_currentCharIndex));
+                    _isPaused = false;
+                    pauseButton.Text = "Pause";
+                }
+            }
+        }
+
+        void Synthesizer_SpeakProgress(object sender, SpeakProgressEventArgs e) => _currentCharIndex = e.CharacterPosition;
+
+        void Synthesizer_SpeakCompleted(object sender, SpeakCompletedEventArgs e)
+        {
+            if (InvokeRequired)
+            {
+                Invoke(new Action(() =>
+                {
+                    stopButton.Enabled = false;
+                    pauseButton.Enabled = false;
+                    playButton.Enabled = true;
+                    talkSpeedSlider.Enabled = true;
+                    talkVolumeSlider.Enabled = true;
+                }));
+            }
+            else
+            {
+                stopButton.Enabled = false;
+                pauseButton.Enabled = false;
+                playButton.Enabled = true;
+                talkSpeedSlider.Enabled = true;
+                talkVolumeSlider.Enabled = true;
+            }
+        }
+
+        void stopButton_Click(object sender, EventArgs e)
+        {
+            _synthesizer.SpeakAsyncCancelAll();
+            _currentCharIndex = 0;
+            _isPaused = false;
+            pauseButton.Text = "Pause";
+            pauseButton.Enabled = false;
+            stopButton.Enabled = false;
+            talkSpeedSlider.Enabled = true;
+            talkVolumeSlider.Enabled = true;
+        }
+
+        void clearPdfButton_Click(object sender, EventArgs e)
+        {
+            if (pdfWebBrowser.CoreWebView2 != null) pdfWebBrowser.CoreWebView2.Navigate("about:blank");
+            _loadedDocument?.Dispose();
+            _loadedDocument = null;
+            loadedPdfTextbox.Clear();
+            startPageTextbox.Clear();
+            endPageTextbox.Clear();            
+            loadedPdfTotalPageLabel.Text = "";
+            displayPdfTextbox.Text = "";
+            _totalPages = 0;
+            _fullTextToRead = "";
+            _currentCharIndex = 0;
+            _isPaused = false;
+            _synthesizer.SpeakAsyncCancelAll();
+            pauseButton.Text = "Pause";
+            playButton.Enabled = false;
+            pauseButton.Enabled = false;
+            stopButton.Enabled = false;
+            talkSpeedSlider.Enabled = true;
+            talkVolumeSlider.Enabled = true;
+        }
+
+        void customTextCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            displayPdfTextbox.ReadOnly = !customTextCheckBox.Checked;
+            if (customTextCheckBox.Checked) displayPdfTextbox.Text = "";
+            else displayPdfTextbox.Text = "";
+            UpdatePlayButtonEnabledState();
+        }
+
+        void displayPdfTextbox_TextChanged(object sender, EventArgs e) => UpdatePlayButtonEnabledState();
+
+        void UpdatePlayButtonEnabledState()
+        {
+            if (customTextCheckBox.Checked) playButton.Enabled = !string.IsNullOrWhiteSpace(displayPdfTextbox.Text);
+            else playButton.Enabled = _loadedDocument != null;
+        }
+    }
+}
